80d703feca04e3dcfbe10321cb1c52e79c4f9aae
jakemerlin1133 <jakemerlin123@gmail.com>
2024-02-13 16:36:46 +0800

    code added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
-    <title>Document</title>
-</head>
-<body>
-
+@include('partials.header')
 
 <div class="mt-64">
 <form class="max-w-sm mx-auto">
@@ -25,8 +15,6 @@
   </form>
 </div>
 
+@include('partials.footer')
 
 
-<script src="../path/to/flowbite/dist/flowbite.min.js"></script>
-</body>
-</html>
